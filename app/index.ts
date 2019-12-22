bc38ef7c737d5cf2b1750e2533aad67cb3152ebb
Joshua Grierson <encredgaming@gmail.com>
2019-12-22 22:23:46 +0100

    [redis] - added connect to edis

--- app/index.ts
+++ app/index.ts
@@ -1,6 +1,7 @@
 import fs from "fs";
 import path from "path";
 import { Server } from "http";
+import Redis from "ioredis";
 import Bluebird from "bluebird";
 
 require("dotenv").config({
@@ -12,6 +13,12 @@ const http: Server = Bluebird.promisifyAll(require("http"));
 const RSA_PRIVATE_KEY_PATH: string = ".rsa/key";
 const RSA_PUBLIC_KEY_PATH: string = ".rsa/key.pub";
 
+enum RedisEventType {
+    error="error",
+    connect="connect",
+    reconnecting="reconnection"
+}
+
 async function testRSAKeys(): Promise<boolean> {
     if (!fs.existsSync(RSA_PRIVATE_KEY_PATH) || !fs.existsSync(RSA_PUBLIC_KEY_PATH)) {
         throw new Error(`RSA key pair not found at path '${RSA_PRIVATE_KEY_PATH}, ${RSA_PUBLIC_KEY_PATH}'`);
@@ -20,4 +27,40 @@ async function testRSAKeys(): Promise<boolean> {
     return Promise.resolve(true);
 }
 
-console.log(process.env.REDIS_HOST);
+async function setupRedis(): Promise<Redis.Redis> {
+    const redis: Redis.Redis = new Redis({
+        port: parseInt(process.env.REDIS_PORT),
+        host: process.env.REDIS_HOST,
+        password: process.env.REDIS_PASS
+    });
+
+    const handler: (type: RedisEventType, err?: string) => void = (type: RedisEventType, err?: any) => {
+        if (type === RedisEventType.error) {
+            console.error(`Redis error occurred: ${err}`);
+        } else if (type === RedisEventType.connect) {
+            console.log(`Redis message connected to: ${process.env.REDIS_HOST}:${process.env.REDIS_PORT}`);
+        } else if (type === RedisEventType.reconnecting) {
+            console.log(`Redis reconnecting to server: ${process.env.REDIS_HOST}:${process.env.REDIS_PORT}`);
+        }
+    }
+
+    redis.on("connect", () => handler(RedisEventType.connect));
+    redis.on("error", (err) => handler(RedisEventType.error, err));
+    redis.on("reconnecting", () => handler(RedisEventType.reconnecting));
+
+    return redis;
+}
+
+async function run() {
+    console.log("Powering up Ingest Service");
+    
+    try {
+        await testRSAKeys();
+        const redisInstance: Redis.Redis = await setupRedis();
+    } catch(ex) {
+        console.error(`Ingest Service error: ${ex.stack}`);
+        process.exit(1);
+    }
+}
+
+run();
